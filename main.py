0b05f28516f0ca097dce66f42adc3de9fa650e1d
Pitkov Denis <den70007.ua@gmail.com>
2021-02-22 09:14:00 +0200

    cleaned code

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-class Mechanical3Dpuzzle:
+class Mechanical3dPuzzle:
 
-    countOfPuzzels=0
+    countOfPuzzles=0
 
-    def __init__(self,countOfElements=0,guaranteeInMonths=0,material="",typeOfPuzzel="",price=None,name=None,countOfBuys=None):
+    def __init__(self,countOfElements=0,guaranteeInMonths=0,material="",typeOfPuzzle="",price=None,name=None,countOfBuys=None):
         self.countOfElements=countOfElements
         self.guaranteeInMonths=guaranteeInMonths
         self.material=material
-        self.typeOfPuzzel=typeOfPuzzel
+        self.typeOfPuzzle=typeOfPuzzle
         self.price=price
         self.name=name
         self.countOfBuys=countOfBuys
-        Mechanical3Dpuzzle.countOfPuzzels=Mechanical3Dpuzzle.countOfPuzzels+1
+        Mechanical3dPuzzle.countOfPuzzles=Mechanical3dPuzzle.countOfPuzzles+1
 
 
     @staticmethod
     def getCountOfPuzzels():
-        return Mechanical3Dpuzzle.countOfPuzzels
+        return Mechanical3dPuzzle.countOfPuzzles
 
     def __del__(self):
         print('Destructor called, object deleted')
 
     def __str__(self):
-        return "Count of elements = {countOfElements}, guarantee = {guaranteeInMonths} Months, material = {material}, type of puzzel = {typeOfPuzzel} {price} {name} {countOfBuys}\n".format(countOfElements=self.countOfElements,
-                                                                                                                                                                                             guaranteeInMonths=self.guaranteeInMonths,
-                                                                                                                                                                                             material=self.material,
-                                                                                                                                                                                             typeOfPuzzel=self.typeOfPuzzel,
-                                                                                                                                                                                             price=f", price = {str(self.price)}" if self.price else "",
-                                                                                                                                                                                             name=f", name = {str(self.name)}" if self.name else "",
-                                                                                                                                                                                             countOfBuys=f", count of buys = {str(self.countOfBuys)}" if self.countOfBuys else "")
+        returneSring="Count of elements = {countOfElements}, guarantee = {guaranteeInMonths} Months, material = {material}, type of puzzel = {typeOfPuzzel} {price} {name} {countOfBuys}\n"
+        return returneSring.format(countOfElements=self.countOfElements,
+                                                                guaranteeInMonths=self.guaranteeInMonths,
+                                                                material=self.material,
+                                                                typeOfPuzzel=self.typeOfPuzzel,
+                                                                price=f", price = {str(self.price)}" if self.price else "",
+                                                                name=f", name = {str(self.name)}" if self.name else "",
+                                                                countOfBuys=f", count of buys = {str(self.countOfBuys)}" if self.countOfBuys else "")
 def main():
-    first = Mechanical3Dpuzzle(124, 12, "Cardboard", "Cars", 499)
-    second = Mechanical3Dpuzzle(256, 6, "Cardboard", "Cartoons", 9999, "Princess")
-    third = Mechanical3Dpuzzle(512, 8, "Cardboard", "Dogs", 1499, "Puppies", 12)
+    first = Mechanical3dPuzzle(124, 12, "Cardboard", "Cars", 499)
+    second = Mechanical3dPuzzle(256, 6, "Cardboard", "Cartoons", 9999, "Princess")
+    third = Mechanical3dPuzzle(512, 8, "Cardboard", "Dogs", 1499, "Puppies", 12)
     print(str(first), str(second), str(third))
-    print(f"count of puzzels : {Mechanical3Dpuzzle.getCountOfPuzzels()}")
+    print(f"count of puzzels : {Mechanical3dPuzzle.getCountOfPuzzels()}")
 
 if __name__ == '__main__':
     main()
